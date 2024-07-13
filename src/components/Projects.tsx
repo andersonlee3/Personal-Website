bdcf7dd23e7cb4a7fe239f28ff94ddeffca4b65a
Anderson Lee <andersonlee@ucsb.edu>
2024-07-13 00:26:15 -0700

    finished timeline

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -18,10 +18,20 @@ const Projects = () => {
           Date: This project was finished in April 2019, my junior year of high
           school.{" "}
         </h3>
-        <h3 className="Inspiration">
+        <h3 className="inspiration">
           Inspiration: I am an athlete. When thinking of a project idea, I was
           motivated to create a fitness app since that I something I would use.
         </h3>
+        <h3 className="implementation">Implementation:</h3>
+        <ul className="list-group">
+          <li className="list-group-item">Implemented using JS and Code.org</li>
+          <li className="list-group-item">
+            Personally designed UI and backend
+          </li>
+          <li className="list-group-item">
+            Assigns different workouts based on user input
+          </li>
+        </ul>
       </div>
 
       <h3 className="inProgress">IN PROGRESS...</h3>
