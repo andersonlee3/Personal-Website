56d7c132021cdcdca767a37b08edb189314c4d8d
Anderson Lee <andersonlee@ucsb.edu>
2024-07-13 00:32:33 -0700

    changed title

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ import About from "./components/About";
 import "./App.css";
 import Journey from "./components/Journey";
 import Projects from "./components/Projects";
+import React, { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    document.title = "Anderson Lee";
+  }, []);
   return (
     <div className="background">
       <About></About>
